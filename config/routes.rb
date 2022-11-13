09c2e5fdf8c2dd9117e8884bc6ba618d20c19c51
EC2 Default User <ec2-user@ip-172-31-39-92.ap-northeast-1.compute.internal>
2022-11-13 14:47:43 +0000

    13章終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :post_images, only:[:new, :create, :index, :show]
+  resources :post_images, only:[:new, :create, :index, :show, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root to: "homes#top"
