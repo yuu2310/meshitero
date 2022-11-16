6ebae056b13b64fb80b6a6234bc604b510f52551
EC2 Default User <ec2-user@ip-172-31-39-92.ap-northeast-1.compute.internal>
2022-11-16 13:48:40 +0000

    14章終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only:[:show, :edit]
   resources :post_images, only:[:new, :create, :index, :show, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
