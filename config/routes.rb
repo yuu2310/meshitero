c7ed1a7dc0ea77004aea3269c74d7032486c3e80
EC2 Default User <ec2-user@ip-172-31-39-92.ap-northeast-1.compute.internal>
2022-11-19 13:54:38 +0000

    18章終了

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get "homes/about" => "homes#about",as: :about
   resources :users, only:[:show, :edit, :update]
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only:[:create, :destroy]
+  end
 end
