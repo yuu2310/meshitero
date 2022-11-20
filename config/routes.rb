00e670e61b6c022aaeca7b2935f6140afb1a45bf
EC2 Default User <ec2-user@ip-172-31-39-92.ap-northeast-1.compute.internal>
2022-11-20 14:32:37 +0000

    19章終了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get "homes/about" => "homes#about",as: :about
   resources :users, only:[:show, :edit, :update]
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resource :favorites, only:[:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
   end
 end
---
+++ test/models/favcorite_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FavcoriteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
